37d35bee3fb155550a7ee958352a4c33fdfd1e3d
baobei5507 <550759734@qq.com>
2020-03-10 13:01:11 +0800

    修复了通知跳转页面的bug

--- src/main/java/life/keke/community/service/CommentService.java
+++ src/main/java/life/keke/community/service/CommentService.java
@@ -97,7 +97,7 @@ public class CommentService {
         Notification notification = new Notification();
         notification.setGmtCreate(System.currentTimeMillis());
         notification.setType(notificationType.getType());
-        notification.setOuter(outerId);
+        notification.setOuterid(outerId);
         notification.setNotifier(comment.getCommentator());
         notification.setStatus(NotificationStatusEnum.UNREAD.getStatus());
         notification.setReceiver(receiver);
